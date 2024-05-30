e6b6e9e27cf4bf80b6e33d5bed04d0642908f1ef
RayHDF <93571820+RayHDF@users.noreply.github.com>
2024-05-31 00:01:15 +0700

    deleted some stuff

--- app/src/main/java/com/example/tubes_dailytracker/MainActivity.kt
+++ app/src/main/java/com/example/tubes_dailytracker/MainActivity.kt
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.android.material.button.MaterialButton
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 
 }
