4e29501e6aaf1bde9a6f2cd0ff715ce758a9646e
RayHDF <93571820+RayHDF@users.noreply.github.com>
2024-05-30 21:56:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tubes_dailytracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tubes_dailytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
