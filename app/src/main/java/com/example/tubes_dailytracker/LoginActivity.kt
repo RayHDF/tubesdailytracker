4f8f7c9e070f47193aa2f3b69dda9ea3f3bcf93e
RayHDF <93571820+RayHDF@users.noreply.github.com>
2024-05-31 11:48:15 +0700

    added login

--- app/src/main/java/com/example/tubes_dailytracker/LoginActivity.kt
+++ app/src/main/java/com/example/tubes_dailytracker/LoginActivity.kt
@@ -1,11 +1,45 @@
 package com.example.tubes_dailytracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+
+        val email = findViewById<EditText>(R.id.et_name)
+        val password = findViewById<EditText>(R.id.et_password)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            val emailInput = email.text.toString()
+            val passwordInput = password.text.toString()
+
+            auth.signInWithEmailAndPassword(emailInput, passwordInput)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            Log.d("LoginActivity", "Email is: $emailInput")
+            Log.d("LoginActivity", "Password is: $passwordInput")
+        }
     }
+
 }
--- app/src/main/java/com/example/tubes_dailytracker/SignupSuccessActivity.kt
+++ app/src/main/java/com/example/tubes_dailytracker/SignupSuccessActivity.kt
@@ -2,10 +2,38 @@ package com.example.tubes_dailytracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignupSuccessActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_success)
+        auth = Firebase.auth
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        val docRef = FirebaseFirestore.getInstance().collection("users").document(userId.toString())
+
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val name = document.getString("name")
+                    val tvNameSus = findViewById<TextView>(R.id.tv_name_sus)
+                    tvNameSus.text = name
+                } else {
+                    Log.d("Firestore", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("Firestore", "get failed with ", exception)
+            }
+
+
     }
 }
