2c2dbdad3041c95766aba136a8a717f85f8dfc86
rehan <rayhd135@gmail.com>
2024-06-10 13:37:19 +0700

    akh

---
+++ app/src/main/java/com/example/tubes_dailytracker/schema/HabitSchema.kt
@@ -0,0 +1,8 @@
+package com.example.tubes_dailytracker.schema
+
+data class HabitSchema (
+    val userID: String = "",
+    val habit_name: String = "",
+    val habit_notes: String = "",
+    val resetCounter: String = ""
+)
