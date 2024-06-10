73d5dc71f042e5df063771089b584450c7b3ebbb
rehan <rayhd135@gmail.com>
2024-06-10 13:28:08 +0700

    akh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation ("com.android.support:appcompat-v7:28.0.0")
 
 
 }
