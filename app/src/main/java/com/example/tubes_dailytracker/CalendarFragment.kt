e778e536287f82b5e1dc2c4766e7583cd8b1305d
FaradayB <faradaybarrf@gmail.com>
2024-06-10 13:10:48 +0700

    Log 8, This is Marin, how tf u customize calendar view ffs

--- app/src/main/java/com/example/tubes_dailytracker/CalendarFragment.kt
+++ app/src/main/java/com/example/tubes_dailytracker/CalendarFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.ListView
 
 // TODO: Rename parameter arguments, choose names that match
