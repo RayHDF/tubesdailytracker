88ace45178635159f991b8d41f8f7484a44bc0e1
rehan <rayhd135@gmail.com>
2024-06-10 18:08:47 +0700

    miracle

--- app/src/main/java/com/example/tubes_dailytracker/CalendarFragment.kt
+++ app/src/main/java/com/example/tubes_dailytracker/CalendarFragment.kt
@@ -1,6 +1,7 @@
 package com.example.tubes_dailytracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import android.widget.Button
 import android.widget.CalendarView
 import android.widget.ListView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,14 +44,54 @@ class CalendarFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
 
         val listView: ListView = view.findViewById(R.id.listView)
-        val data = listOf("Item 1", "Item 2", "Item 3", "Item 4").toMutableList()
-        val adapter = CustomAdapter(requireContext(), R.layout.list_item, data)
-        listView.adapter = adapter
+        val data = mutableListOf<String>()
+
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
+
+        val userID = auth.currentUser?.uid ?: "default"
+        db.collection("task")
+            .whereEqualTo("userID", userID)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val item = document.getString("task_name")
+                    if (item != null) {
+                        data.add(item)
+                    }
+                }
+                val adapter = CustomAdapter(requireContext(), R.layout.list_item, data)
+                listView.adapter = adapter
+            }
+            .addOnFailureListener { exception ->
+                Log.w("HomeFragment", "Error getting documents: ", exception)
+            }
+
 
         val calendar = view.findViewById<CalendarView>(R.id.cv_tasks)
 
         calendar.setOnDateChangeListener{ view, year, month, dayOfMonth ->
-            val date = "$dayOfMonth/${month + 1}/$year"
+            val date = "${month + 1}/$dayOfMonth/$year"
+
+            db.collection("task")
+                .whereEqualTo("userID", userID)
+                .whereEqualTo("task_schedule", date)
+                .get()
+                .addOnSuccessListener { documents ->
+                    data.clear() // clear the old data
+                    for (document in documents) {
+                        val item = document.getString("task_name") // replace "item" with the actual field name
+                        if (item != null) {
+                            data.add(item)
+                        }
+                    }
+                    Log.w("CalendarFragment", "Date: $date")
+                    val adapter = CustomAdapter(requireContext(), R.layout.list_item, data)
+                    listView.adapter = adapter
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("CalendarFragment", "Error getting documents: ", exception)
+                }
 
         }
 
